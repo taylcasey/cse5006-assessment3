c0c3a6e8cd1a45fea9348aa031b0b174e7af466a
Taylor C. <taylcasey@gmail.com>
2026-08-19 21:17:40 +1000

    update playwright config to ensure client and server side tests do not run simultaneously and conflict

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
     testDir: './tests',
     timeout: 30000,
+    workers: 1,
+    fullyParallel: false,
     use: {
         baseURL: 'http://localhost:3000',
     },
